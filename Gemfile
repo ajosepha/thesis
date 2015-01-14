6ff9aa116be49473d63ee2096c397ef4579618ac
Ariel Isaacs <ariel.isaacs@gmail.com>
2015-01-13 18:31:33 -0800

    building gems

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source "https://rubygems.org"
 
+group :production do
+  gem 'pg'
+end
 
 gem "sinatra"
 gem "activerecord"
---
+++ vendor/bundle/ruby/2.0.0/gems/pg-0.18.1/ext/siteconf20150113-29191-qthp6q.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20150113-29191-18m1ryo"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
